56fe66ca3651a525244f03a9fc787f8ed2faa2f1
Paulo Henrique Correa dos Santos <paulo.santos@engdb.com.br>
2019-09-20 17:37:57 -0300

    Java 8 - Method Reference

--- src/com/ph/java8/defaultMethods/OrdenaStrings.java
+++ src/com/ph/java8/defaultMethods/OrdenaStrings.java
@@ -5,6 +5,7 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.function.Consumer;
+import java.util.function.Function;
 
 public class OrdenaStrings {
 
@@ -23,7 +24,22 @@ public class OrdenaStrings {
 //		palavras.sort(comparador);
 //		System.out.println(palavras);
 		
-		palavras.sort((s1,s2) -> Integer.compare(s1.length() ,s2.length()));
+//		palavras.sort((s1,s2) -> Integer.compare(s1.length() ,s2.length()));
+		
+//		palavras.sort(Comparator.comparing(s -> s.length()));
+		
+		palavras.sort(Comparator.comparing(String::length));
+		
+		Function<String, Integer> funcao = new Function<String, Integer>() {
+			@Override
+			public Integer apply(String s) {
+				return s.length();
+			}
+		};
+		Comparator<String> comparador = Comparator.comparing(funcao);
+		palavras.sort(comparador);
+		
+		
 		System.out.println(palavras);
 		
 //		Consumer<String> consumidor = new ConsumidorDeString();
@@ -36,9 +52,12 @@ public class OrdenaStrings {
 //			}
 //		});
 		
-		palavras.forEach(s -> System.out.println(s));
+//		palavras.forEach(s -> System.out.println(s));
+		
+		palavras.forEach(System.out::println);
 		
 		new Thread(() -> System.out.println("Executando um Runnable")).start();
+		
 	}
 	
 }
